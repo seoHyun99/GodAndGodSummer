98823ecc3e8d552d11d2cd73c6f5d670b10026eb
seoHyun99 <seu1140@naver.com>
2016-10-14 16:32:18 +0900

    2016-10-14

---
+++ app/src/main/java/com/juntcompany/godandgodsummer/Dialog/NoMatchEmailDialogFragment.java
@@ -0,0 +1,43 @@
+package com.juntcompany.godandgodsummer.Dialog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.juntcompany.godandgodsummer.R;
+
+
+public class NoMatchEmailDialogFragment extends DialogFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.MyDialog);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_no_match_email_dialog, container, false);
+
+        final Button btn_research = (Button)view.findViewById(R.id.button_reserch);
+
+        setCancelable(true);
+
+        btn_research.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), LoginHelp_FindEmail.class);
+                startActivity(i);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // Todo: 확인해보기
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/juntcompany/godandgodsummer/Dialog/NoMatchPhoneDialogFragment.java
@@ -0,0 +1,47 @@
+package com.juntcompany.godandgodsummer.Dialog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.juntcompany.godandgodsummer.R;
+
+
+public class NoMatchPhoneDialogFragment extends DialogFragment {
+    public NoMatchPhoneDialogFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.MyDialog);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_no_match_phone_number_dialog, container, false);
+
+        final Button btn_reserch = (Button) view.findViewById(R.id.button_reserch);
+
+        setCancelable(true);
+        getDialog().setCanceledOnTouchOutside(true);
+
+        btn_reserch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), LoginHelp_FindPhone.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); //Todo: 확인해보기.
+                startActivity(i);
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+}
