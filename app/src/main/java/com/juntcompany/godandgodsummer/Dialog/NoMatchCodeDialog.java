fe3a1d17c53ffd48f829885b85755b8391699bf5
seoHyun99 <seu1140@naver.com>
2016-10-20 20:44:10 +0900

    2016-10-20

---
+++ app/src/main/java/com/juntcompany/godandgodsummer/Dialog/NoMatchCodeDialog.java
@@ -0,0 +1,60 @@
+package com.juntcompany.godandgodsummer.Dialog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by 이서현 on 2016-09-29.
+ */
+public class NoMatchCodeDialog extends DialogFragment {
+
+    public NoMatchCodeDialog() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.MyDialog);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Intent intent = getActivity().getIntent();
+        final int index = intent.getIntExtra("index", 0);
+
+        View view = inflater.inflate(R.layout.fragment_no_match_code_dialog, container, false);
+
+        final Button btn_retry = (Button) view.findViewById(R.id.button_retry); // 다시 시도 새로 화면 출력.
+        final Button btn_revest = (Button) view.findViewById(R.id.button_revestCode); // 다시 받기
+
+        setCancelable(true);
+        getDialog().setCanceledOnTouchOutside(true);
+
+        btn_retry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), getActivity().getClass());
+                i.putExtra("index", index);
+                startActivity(i);
+                dismiss();
+            }
+        });
+        btn_revest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // 다이얼로그만 끄기
+                ((LoginHelp_confirm_account) LoginHelp_confirm_account.mContext).sendCodeToUser();
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/juntcompany/godandgodsummer/Login/Help/HelpCreatePasswordActivity.java
@@ -0,0 +1,96 @@
+package com.juntcompany.godandgodsummer.Login.Help;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class HelpCreatePasswordActivity extends AppCompatActivity {
+
+    private final String Passwrod_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=[\\S]+$).{6,20}";
+
+    private EditText inputPassWord;
+    private Button complete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_password);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.button_back);
+
+        View viewToolbar = getLayoutInflater().inflate(R.layout.toolbar_login_help, null);
+        TextView textTitle = (TextView) viewToolbar.findViewById(R.id.toolbar_text);
+        textTitle.setText("계정 비밀번호");
+
+        complete = (Button) findViewById(R.id.complete);
+        inputPassWord = (EditText) findViewById(R.id.inputPassWord);
+        inputPassWord.addTextChangedListener(textWatcherInput);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                goActivity(HelpActivity.class);
+            }
+        });
+
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!Passwordvalidate(inputPassWord.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "비밀번호를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    //Todo: 로그인 첫 화면으로 이동.
+                }
+            }
+        });
+    }
+
+    private boolean Passwordvalidate(final String hex) {
+        Pattern pattern = Pattern.compile(Passwrod_PATTERN);
+        Matcher matcher = pattern.matcher(hex);
+        return matcher.matches();
+    }
+
+    private void goActivity(Class c){
+        Intent i = new Intent(this, c);
+        startActivity(i);
+        finish();
+    }
+
+    TextWatcher textWatcherInput = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(inputPassWord.getText().toString().length() > 5 && inputPassWord.getText().toString().length() < 21)
+                complete.setVisibility(View.VISIBLE);
+            else
+                complete.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+}
